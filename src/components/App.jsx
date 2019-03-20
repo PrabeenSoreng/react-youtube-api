580fccbf316533acb68dd07c9d4f876c039a147a
Prabeen <prabeensoreng2119@gmail.com>
2019-03-20 21:09:10 +0530

    final push

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,11 @@ import VideoList from "./VideoList";
 import youtube from "../apis/youtube";
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
+
+  componentDidMount() {
+    this.onTermSubmit("hollywood music");
+  }
 
   onTermSubmit = async term => {
     const response = await youtube.get("/search", {
@@ -13,15 +17,33 @@ class App extends React.Component {
         q: term
       }
     });
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    });
+  };
+
+  onVideoSelect = video => {
+    this.setState({ selectedVideo: video });
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoDetail />
-        <VideoList />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="ten wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="six wide column">
+              <VideoList
+                videos={this.state.videos}
+                onVideoSelect={this.onVideoSelect}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import VideoItem from "./VideoItem";
 
 class VideoList extends React.Component {
   render() {
-    return <div>Video List</div>;
+    const renderedList = this.props.videos.map(video => {
+      return (
+        <VideoItem
+          key={video.id.videoId}
+          video={video}
+          onVideoSelect={this.props.onVideoSelect}
+        />
+      );
+    });
+    return <div className="ui relaxed divided list">{renderedList}</div>;
   }
 }
 
